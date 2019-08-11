f9d07532edee7eb91553b2bdb53f51aec4a2108b
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-08-12 00:00:00 +0200

    Cleaned block Browse Preview.

--- src/Form/BrowsePreviewFieldset.php
+++ src/Form/BrowsePreviewFieldset.php
@@ -9,65 +9,66 @@ class BrowsePreviewFieldset extends Fieldset
 {
     public function init()
     {
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][resource_type]',
-            'type' => Element\Select::class,
-            'options' => [
-                'label' => 'Resource type', // @translate
-                'value_options' => [
-                    'items' => 'Items', // @translate
-                    'item_sets' => 'Item sets', // @translate
-                    'media' => 'Media', // @translate
+        $this
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][resource_type]',
+                'type' => Element\Select::class,
+                'options' => [
+                    'label' => 'Resource type', // @translate
+                    'value_options' => [
+                        'items' => 'Items', // @translate
+                        'item_sets' => 'Item sets', // @translate
+                        'media' => 'Media', // @translate
+                    ],
                 ],
-            ],
-            'attributes' => [
-                'class' => 'chosen-select',
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][query]',
-            'type' => Element\Text::class,
-            'options' => [
-                'label' => 'Query', // @translate
-                'info' => 'Display resources using this search query', // @translate
-                'documentation' => 'https://omeka.org/s/docs/user-manual/sites/site_pages/#browse-preview',
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][limit]',
-            'type' => Element\Number::class,
-            'options' => [
-                'label' => 'Limit', // @translate
-                'info' => 'Maximum number of resources to display in the preview.', // @translate
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][heading]',
-            'type' => Element\Text::class,
-            'options' => [
-                'label' => 'Block title', // @translate
-                'info' => 'Heading for the block, if any.', // @translate
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][link-text]',
-            'type' => Element\Text::class,
-            'options' => [
-                'label' => 'Link text', // @translate
-                'info' => 'Text for link to full browse view, if any. There is no link for media.', // @translate
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][partial]',
-            'type' => PartialSelect::class,
-            'options' => [
-                'label' => 'Partial to display', // @translate
-                'info' => 'Partials are in folder "common/block-layout" of the theme and should start with "browse-preview".', // @translate
-                'partial' => 'common/block-layout/browse-preview',
-            ],
-            'attributes' => [
-                'class' => 'chosen-select',
-            ],
-        ]);
+                'attributes' => [
+                    'class' => 'chosen-select',
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][query]',
+                'type' => Element\Text::class,
+                'options' => [
+                    'label' => 'Query', // @translate
+                    'info' => 'Display resources using this search query', // @translate
+                    'documentation' => 'https://omeka.org/s/docs/user-manual/sites/site_pages/#browse-preview',
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][limit]',
+                'type' => Element\Number::class,
+                'options' => [
+                    'label' => 'Limit', // @translate
+                    'info' => 'Maximum number of resources to display in the preview.', // @translate
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][heading]',
+                'type' => Element\Text::class,
+                'options' => [
+                    'label' => 'Block title', // @translate
+                    'info' => 'Heading for the block, if any.', // @translate
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][link-text]',
+                'type' => Element\Text::class,
+                'options' => [
+                    'label' => 'Link text', // @translate
+                    'info' => 'Text for link to full browse view, if any. There is no link for media.', // @translate
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][partial]',
+                'type' => PartialSelect::class,
+                'options' => [
+                    'label' => 'Partial to display', // @translate
+                    'info' => 'Partials are in folder "common/block-layout" of the theme and should start with "browse-preview".', // @translate
+                    'partial' => 'common/block-layout/browse-preview',
+                ],
+                'attributes' => [
+                    'class' => 'chosen-select',
+                ],
+            ]);
     }
 }
