3ca19103e00ecfa1ed52b2d5c3d19cebc4ec751d
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-08-26 00:00:00 +0200

    Cleaned code.

--- src/Form/ItemShowcaseFieldset.php
+++ src/Form/ItemShowcaseFieldset.php
@@ -10,35 +10,38 @@ class ItemShowcaseFieldset extends Fieldset
 {
     public function init()
     {
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][heading]',
-            'type' => Element\Text::class,
-            'options' => [
-                'label' => 'Block title', // @translate
-                'info' => 'Heading for the block, if any.', // @translate
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][thumbnail_type]',
-            'type' => ThumbnailTypeSelect::class,
-            'options' => [
-                'label' => 'Thumbnail type', // @translate
-            ],
-            'attributes' => [
-                'class' => 'chosen-select',
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][partial]',
-            'type' => PartialSelect::class,
-            'options' => [
-                'label' => 'Partial to display', // @translate
-                'info' => 'Partials are in folder "common/block-layout" of the theme and should start with "item-showcase".', // @translate
-                'partial' => 'common/block-layout/item-showcase',
-            ],
-            'attributes' => [
-                'class' => 'chosen-select',
-            ],
-        ]);
+        // Attachments fields are managed separately.
+
+        $this
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][heading]',
+                'type' => Element\Text::class,
+                'options' => [
+                    'label' => 'Block title', // @translate
+                    'info' => 'Heading for the block, if any.', // @translate
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][thumbnail_type]',
+                'type' => ThumbnailTypeSelect::class,
+                'options' => [
+                    'label' => 'Thumbnail type', // @translate
+                ],
+                'attributes' => [
+                    'class' => 'chosen-select',
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][partial]',
+                'type' => PartialSelect::class,
+                'options' => [
+                    'label' => 'Partial to display', // @translate
+                    'info' => 'Partials are in folder "common/block-layout" of the theme and should start with "item-showcase".', // @translate
+                    'partial' => 'common/block-layout/item-showcase',
+                ],
+                'attributes' => [
+                    'class' => 'chosen-select',
+                ],
+            ]);
     }
 }
--- src/Form/ResourceTextFieldset.php
+++ src/Form/ResourceTextFieldset.php
@@ -12,86 +12,87 @@ class ResourceTextFieldset extends Fieldset
     {
         // Attachments fields are managed separately.
 
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][thumbnail_type]',
-            'type' => ThumbnailTypeSelect::class,
-            'options' => [
-                'label' => 'Thumbnail type', // @translate
-            ],
-            'attributes' => [
-                'class' => 'chosen-select',
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][alignment]',
-            'type' => Element\Select::class,
-            'options' => [
-                'label' => 'Thumbnail alignment', // @translate
-                'value_options' => [
-                    'left' => 'left', // @translate
-                    'right' => 'right', // @translate
+        $this
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][thumbnail_type]',
+                'type' => ThumbnailTypeSelect::class,
+                'options' => [
+                    'label' => 'Thumbnail type', // @translate
                 ],
-            ],
-            'attributes' => [
-                'class' => 'chosen-select',
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][show_title_option]',
-            'type' => Element\Select::class,
-            'options' => [
-                'label' => 'Show attachment title', // @translate
-                'value_options' => [
-                    'item_title' => 'item title', // @translate
-                    'file_name' => 'media title', // @translate
-                    'no_title' => 'no title', // @translate
+                'attributes' => [
+                    'class' => 'chosen-select',
                 ],
-            ],
-            'attributes' => [
-                'class' => 'chosen-select',
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][caption_position]',
-            'type' => Element\Select::class,
-            'options' => [
-                'label' => 'Captions position', // @translate
-                'value_options' => [
-                    'center' => 'center', // @translate
-                    'left' => 'left', // @translate
-                    'right' => 'right', // @translate
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][alignment]',
+                'type' => Element\Select::class,
+                'options' => [
+                    'label' => 'Thumbnail alignment', // @translate
+                    'value_options' => [
+                        'left' => 'left', // @translate
+                        'right' => 'right', // @translate
+                    ],
                 ],
-            ],
-            'attributes' => [
-                'class' => 'chosen-select',
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][heading]',
-            'type' => Element\Text::class,
-            'options' => [
-                'label' => 'Block title', // @translate
-                'info' => 'Heading for the block, if any.', // @translate
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][html]',
-            'type' => Element\Textarea::class,
-            'attributes' => [
-                'class' => 'block-html full wysiwyg',
-            ],
-        ]);
-        $this->add([
-            'name' => 'o:block[__blockIndex__][o:data][partial]',
-            'type' => PartialSelect::class,
-            'options' => [
-                'label' => 'Partial to display', // @translate
-                'info' => 'Partials are in folder "common/block-layout" of the theme and should start with "resource-text".', // @translate
-                'partial' => 'common/block-layout/resource-text',
-            ],
-            'attributes' => [
-                'class' => 'chosen-select',
-            ],
-        ]);
+                'attributes' => [
+                    'class' => 'chosen-select',
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][show_title_option]',
+                'type' => Element\Select::class,
+                'options' => [
+                    'label' => 'Show attachment title', // @translate
+                    'value_options' => [
+                        'item_title' => 'item title', // @translate
+                        'file_name' => 'media title', // @translate
+                        'no_title' => 'no title', // @translate
+                    ],
+                ],
+                'attributes' => [
+                    'class' => 'chosen-select',
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][caption_position]',
+                'type' => Element\Select::class,
+                'options' => [
+                    'label' => 'Captions position', // @translate
+                    'value_options' => [
+                        'center' => 'center', // @translate
+                        'left' => 'left', // @translate
+                        'right' => 'right', // @translate
+                    ],
+                ],
+                'attributes' => [
+                    'class' => 'chosen-select',
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][heading]',
+                'type' => Element\Text::class,
+                'options' => [
+                    'label' => 'Block title', // @translate
+                    'info' => 'Heading for the block, if any.', // @translate
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][html]',
+                'type' => Element\Textarea::class,
+                'attributes' => [
+                    'class' => 'block-html full wysiwyg',
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][partial]',
+                'type' => PartialSelect::class,
+                'options' => [
+                    'label' => 'Partial to display', // @translate
+                    'info' => 'Partials are in folder "common/block-layout" of the theme and should start with "resource-text".', // @translate
+                    'partial' => 'common/block-layout/resource-text',
+                ],
+                'attributes' => [
+                    'class' => 'chosen-select',
+                ],
+            ]);
     }
 }
