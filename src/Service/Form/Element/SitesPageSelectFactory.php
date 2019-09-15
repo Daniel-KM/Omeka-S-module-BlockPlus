28a2959047fca22c054daffbcb7f2766b3009c4c
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-09-16 00:00:00 +0200

    Added option for SitePageSelect to limit to selected sites.

--- src/Service/Form/Element/SitesPageSelectFactory.php
+++ src/Service/Form/Element/SitesPageSelectFactory.php
@@ -9,8 +9,11 @@ class SitesPageSelectFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
+        $currentSite = $services->get('ControllerPluginManager')->get('currentSite');
+
         $element = new SitesPageSelect(null, $options);
         $element->setApiManager($services->get('Omeka\ApiManager'));
+        $element->setSite($currentSite());
         return $element;
     }
 }
