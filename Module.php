5d50649365a342f958e780f4cba905842c9010cc
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-01-20 00:00:00 +0100

    Used Generic AbstractModule.

--- Module.php
+++ Module.php
@@ -1,26 +1,37 @@
 <?php
 namespace BlockPlus;
 
-use Omeka\Module\AbstractModule;
-use Zend\ServiceManager\ServiceLocatorInterface;
+if (!class_exists(\Generic\AbstractModule::class)) {
+    require file_exists(dirname(__DIR__) . '/Generic/AbstractModule.php')
+        ? dirname(__DIR__) . '/Generic/AbstractModule.php'
+        : __DIR__ . '/src/Generic/AbstractModule.php';
+}
+
+use Generic\AbstractModule;
+use Omeka\Module\Exception\ModuleCannotInstallException;
 
 /**
  * BlockPlus
  *
- * @copyright Daniel Berthereau, 2018-2019
+ * @copyright Daniel Berthereau, 2018-2020
  * @license http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
  */
 class Module extends AbstractModule
 {
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
+    const NAMESPACE = __NAMESPACE__;
 
-    public function upgrade($oldVersion, $newVersion, ServiceLocatorInterface $serviceLocator)
+    protected function preInstall()
     {
-        $filepath = __DIR__ . '/data/scripts/upgrade.php';
-        $this->setServiceLocator($serviceLocator);
-        require_once $filepath;
+        $js = __DIR__ . '/asset/vendor/ThumbnailGridExpandingPreview/js/grid.js';
+        if (!file_exists($js)) {
+            $services = $this->getServiceLocator();
+            $t = $services->get('MvcTranslator');
+            throw new ModuleCannotInstallException(
+                sprintf(
+                    $t->translate('The library "%s" should be installed.'), // @translate
+                    'javascript'
+                ) . ' '
+                . $t->translate('See module’s installation documentation.')); // @translate
+        }
     }
 }
