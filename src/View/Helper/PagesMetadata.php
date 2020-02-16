9422dd76fc98be5b07339e8ab6450d790d78e8da
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-02-17 00:00:00 +0100

    Allowed to get multiple page types once with pagesMetadata().

--- src/View/Helper/PagesMetadata.php
+++ src/View/Helper/PagesMetadata.php
@@ -11,11 +11,13 @@ class PagesMetadata extends AbstractHelper
     /**
      * Get data for all pages of the specified type in the current site.
      *
-     * @param string $pageType
+     * @param string|array $pageType
      * @return \Omeka\Api\Representation\SitePageBlockRepresentation[]
      */
     public function __invoke($pageType)
     {
+        $pageTypes = is_array($pageType) ? $pageType : [$pageType];
+
         $pageBlocks = [];
 
         // Check if the site page has the specified block.
@@ -24,7 +26,7 @@ class PagesMetadata extends AbstractHelper
         foreach ($pages as $page) {
             foreach ($page->blocks() as $block) {
                 // A page can belong to multiple types…
-                if ($block->layout() === 'pageMetadata' && $block->dataValue('type') === $pageType) {
+                if ($block->layout() === 'pageMetadata' && in_array($block->dataValue('type'), $pageTypes)) {
                     $pageBlocks[$page->slug()] = $block;
                     break;
                 }
