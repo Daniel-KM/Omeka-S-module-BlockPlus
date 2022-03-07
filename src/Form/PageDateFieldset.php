a5c91ee53a8b33eec0d03c4142b6631a7f104ba3
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-03-07 00:00:00 +0000

    Added a block to display the dates of a page.

---
+++ src/Form/PageDateFieldset.php
@@ -0,0 +1,87 @@
+<?php declare(strict_types=1);
+
+namespace BlockPlus\Form;
+
+use BlockPlus\Form\Element\TemplateSelect;
+use Laminas\Form\Element;
+use Laminas\Form\Fieldset;
+use Omeka\View\Helper\I18n;
+
+class PageDateFieldset extends Fieldset
+{
+    public function init(): void
+    {
+        $this
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][heading]',
+                'type' => Element\Text::class,
+                'options' => [
+                    'label' => 'Block title', // @translate
+                ],
+                'attributes' => [
+                    'id' => 'page-date-heading',
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][dates]',
+                'type' => Element\Radio::class,
+                'options' => [
+                    'label' => 'Dates', // @translate
+                    'value_options' => [
+                        'created' => 'Created', // @translate
+                        'modified' => 'Updated', // @translate
+                        'created_and_modified' => 'Created and updated', // @translate
+                    ],
+                ],
+                'attributes' => [
+                    'id' => 'page-date-dates',
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][format_date]',
+                'type' => Element\Radio::class,
+                'options' => [
+                    'label' => 'Format date', // @translate
+                    'value_options' => [
+                        I18n::DATE_FORMAT_NONE => 'None', // @translate
+                        I18n::DATE_FORMAT_SHORT => 'Short', // @translate
+                        I18n::DATE_FORMAT_MEDIUM => 'Medium', // @translate
+                        I18n::DATE_FORMAT_LONG => 'Long', // @translate
+                        I18n::DATE_FORMAT_FULL => 'Full', // @translate
+                    ],
+                ],
+                'attributes' => [
+                    'id' => 'page-date-format-date',
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][format_time]',
+                'type' => Element\Radio::class,
+                'options' => [
+                    'label' => 'Format time', // @translate
+                    'value_options' => [
+                        I18n::DATE_FORMAT_NONE => 'None', // @translate
+                        I18n::DATE_FORMAT_SHORT => 'Short', // @translate
+                        I18n::DATE_FORMAT_MEDIUM => 'Medium', // @translate
+                        I18n::DATE_FORMAT_LONG => 'Long', // @translate
+                        I18n::DATE_FORMAT_FULL => 'Full', // @translate
+                    ],
+                ],
+                'attributes' => [
+                    'id' => 'page-date-format-time',
+                ],
+            ])
+            ->add([
+                'name' => 'o:block[__blockIndex__][o:data][template]',
+                'type' => TemplateSelect::class,
+                'options' => [
+                    'label' => 'Template to display', // @translate
+                    'info' => 'Templates are in folder "common/block-layout" of the theme and should start with "page-date".', // @translate
+                    'template' => 'common/block-layout/page-date',
+                ],
+                'attributes' => [
+                    'class' => 'chosen-select',
+                ],
+            ]);
+    }
+}
---
+++ src/Site/BlockLayout/PageDate.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace BlockPlus\Site\BlockLayout;
+
+use Laminas\View\Renderer\PhpRenderer;
+use Omeka\Api\Representation\SitePageBlockRepresentation;
+use Omeka\Api\Representation\SitePageRepresentation;
+use Omeka\Api\Representation\SiteRepresentation;
+use Omeka\Site\BlockLayout\AbstractBlockLayout;
+
+class PageDate extends AbstractBlockLayout
+{
+    /**
+     * The default partial view script.
+     */
+    const PARTIAL_NAME = 'common/block-layout/page-date';
+
+    public function getLabel()
+    {
+        return 'Page date'; // @translate
+    }
+
+    public function form(
+        PhpRenderer $view,
+        SiteRepresentation $site,
+        SitePageRepresentation $page = null,
+        SitePageBlockRepresentation $block = null
+    ) {
+        // Factory is not used to make rendering simpler.
+        $services = $site->getServiceLocator();
+        $formElementManager = $services->get('FormElementManager');
+        $defaultSettings = $services->get('Config')['blockplus']['block_settings']['pageDate'];
+        $blockFieldset = \BlockPlus\Form\PageDateFieldset::class;
+
+        $data = $block ? $block->data() + $defaultSettings : $defaultSettings;
+
+        $dataForm = [];
+        foreach ($data as $key => $value) {
+            $dataForm['o:block[__blockIndex__][o:data][' . $key . ']'] = $value;
+        }
+
+        $fieldset = $formElementManager->get($blockFieldset);
+        $fieldset->populateValues($dataForm);
+
+        return $view->formCollection($fieldset);
+    }
+
+    public function render(PhpRenderer $view, SitePageBlockRepresentation $block)
+    {
+        $vars = $block->data();
+        $vars = [
+            'block' => $block,
+            'heading' => $vars['heading'],
+            'dates' => $vars['dates'],
+            'formatDate' => $vars['format_date'],
+            'formatTime' => $vars['format_time'],
+        ];
+        $template = $block->dataValue('template', self::PARTIAL_NAME);
+        return $template !== self::PARTIAL_NAME && $view->resolver($template)
+            ? $view->partial($template, $vars)
+            : $view->partial(self::PARTIAL_NAME, $vars);
+    }
+}
