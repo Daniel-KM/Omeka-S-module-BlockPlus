599b1de02fdf709b6da85230ff59c7a4a67e7711
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-03-21 00:00:00 +0000

    Updated site-page-edit.js.

--- asset/js/site-page-edit.js
+++ asset/js/site-page-edit.js
@@ -145,6 +145,14 @@
          $('.selected-page + a').attr('href', pageUrl);
      }
 
+    // Prevent drop into ckeditors to avoid picking up block content when sorting
+    CKEDITOR.on('instanceReady', function(e) {
+        var editor = e.editor;
+        editor.editable().attachListener(editor.container, 'drop', function (dropEv) {
+            dropEv.data.preventDefault();
+        })
+    });
+
     $(document).ready(function () {
         var list = document.getElementById('blocks');
         var blockIndex = 0;
@@ -407,6 +415,7 @@
         $('#content').on('click', '#asset-options-confirm-panel', function() {
             var selectingAttachment = $('.selecting.attachment');
             selectingAttachment.removeClass('new');
+            selectingAttachment.find('input[type="hidden"').removeAttr('disabled');
             populateAssetAttachment(selectingAttachment);
             Omeka.closeSidebar($('#asset-options'));
             $('.selecting.attachment').removeClass('selecting');
@@ -419,29 +428,13 @@
         $('#content').on('click', '.page-select', function() {
             var sidebar = $('#page-list');
             var pageList = $('#page-list .pages');
-            var optionTemplate = $('#page-list .option.template');
-
             Omeka.openSidebar(sidebar);
-            var apiUrl = sidebar.data('api-url');
-            if (pageList.find('.option').length == 1) {
-                $.get(apiUrl, function(data) {
-                    data['o:page'].forEach(function(page) {
-                        var newButton = optionTemplate.clone();
-                        $.get(page['@id'], function(pageData) {
-                            newButton.text(pageData['o:title']).val(pageData['o:id']);
-                            newButton.data('page-slug', pageData['o:slug']);
-                        });
-                        pageList.append(newButton);
-                        newButton.removeClass('template');
-                    });
-                }).done(function() {
-                    // Update attachment options sidebar after selecting item.
-                    pageList.on('click', 'button.option', function(e) {
-                        Omeka.closeSidebar($('#page-list'));
-                        selectPageLink($(this));
-                    });
-                });
-            }
+
+            pageList.on('click', 'button.option', function(e) {
+                Omeka.closeSidebar(sidebar);
+                selectPageLink($(this));
+            });
+
         });
 
         $('#content').on('click', '.page-clear', function() {
