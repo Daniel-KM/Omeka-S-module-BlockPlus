8d29a52af28bb722582dfc186a77a77ce677acab
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-03-03 00:00:00 +0000

    Renamed the option "thumbnail_size" by "thumbnail_type" in template browse preview gallery.

--- config/module.config.php
+++ config/module.config.php
@@ -157,7 +157,8 @@ return [
         'browsePreview' => [
             'browse-preview-carousel' => 'Block Plus: Carousel', // @translate
             'browse-preview-filter-year' => 'Block Plus: Filter by year', // @translate
-            'browse-preview-gallery' => 'Block Plus: Gallery', // @translate
+            'browse-preview-gallery' => 'Block Plus: Gallery (square)', // @translate
+            'browse-preview-gallery-medium' => 'Block Plus: Gallery (medium)', // @translate
             'browse-preview-subjects' => 'Block Plus: Subjects', // @translate
             'browse-preview-timeline-list' => 'Block Plus: Timeline list', // @translate
             'browse-preview-deprecated' => 'Block Plus: Browse preview (deprecated)', // @translate
