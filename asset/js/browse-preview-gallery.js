f33807c822dd800f13d7c8ed419f92b223478ec3
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-03-03 00:00:00 +0000

    Updated composer and gallery.

--- asset/js/browse-preview-gallery.js
+++ asset/js/browse-preview-gallery.js
@@ -1,15 +1,29 @@
-$(document).ready(function() {
+'use strict';
 
-$(function() {
-    Grid.init({
-        showVisitButton: false,
+(function() {
+    document.addEventListener('DOMContentLoaded', function() {
+
+        Grid.init({
+            showVisitButton: false,
+        });
+
+        // TODO Fill items from Omeka api.
+        const addItemsEl = document.getElementById('og-additems');
+        if (addItemsEl) {
+            addItemsEl.addEventListener('click', function() {
+                // Add items html here.
+                var itemsHtml = '';
+                var tempDiv = document.createElement('div');
+                tempDiv.innerHTML = itemsHtml;
+                var items = tempDiv.children;
+                var grid = document.getElementById('og-grid');
+                while (items.length > 0) {
+                    grid.appendChild(items[0]);
+                }
+                Grid.addItems(grid.children);
+                return false;
+            });
+        }
+
     });
-    // TODO Fill items from Omeka api.
-    $('#og-additems').on( 'click', function() {
-        var $items = $( '' ).appendTo( $( '#og-grid' ) );
-        Grid.addItems( $items );
-        return false;
-    } );
-});
-
-});
+})();
