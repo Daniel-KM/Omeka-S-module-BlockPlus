f009911b95178b1bb78399134c1f0fce3331d4be
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-07-21 00:00:00 +0000

    Updated gallery for browse-preview.

--- asset/js/browse-preview-gallery.js
+++ asset/js/browse-preview-gallery.js
@@ -1,29 +1,27 @@
 'use strict';
 
-(function() {
-    document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
 
-        Grid.init({
-            showVisitButton: false,
-        });
-
-        // TODO Fill items from Omeka api.
-        const addItemsEl = document.getElementById('og-additems');
-        if (addItemsEl) {
-            addItemsEl.addEventListener('click', function() {
-                // Add items html here.
-                var itemsHtml = '';
-                var tempDiv = document.createElement('div');
-                tempDiv.innerHTML = itemsHtml;
-                var items = tempDiv.children;
-                var grid = document.getElementById('og-grid');
-                while (items.length > 0) {
-                    grid.appendChild(items[0]);
-                }
-                Grid.addItems(grid.children);
-                return false;
-            });
-        }
-
+    Grid.init({
+        showVisitButton: false,
     });
-})();
+
+    // TODO Fill items from Omeka api.
+    const addItemsEl = document.getElementById('og-additems');
+    if (addItemsEl) {
+        addItemsEl.addEventListener('click', function() {
+            // Add items html here.
+            var itemsHtml = '';
+            var tempDiv = document.createElement('div');
+            tempDiv.innerHTML = itemsHtml;
+            var items = tempDiv.children;
+            var grid = document.getElementById('og-grid');
+            while (items.length > 0) {
+                grid.appendChild(items[0]);
+            }
+            Grid.addItems(grid.children);
+            return false;
+        });
+    }
+
+});
